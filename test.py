596ddf4072c6a2130e133f0b44a29f5f10324edc
parsaT79 <parsa.taraj@gmail.com>
2025-08-29 17:30:00 +0300

    parsa added the group B homework (user management v0.3)

---
+++ test.py
@@ -0,0 +1,18 @@
+# "project user management(check up) test"
+# "v0.3"
+
+# the menu
+    # pass
+
+# add user
+    # validation
+        # pass
+
+    # list
+        # pass
+
+# login
+    # all options pass
+
+# show list
+    # pass
